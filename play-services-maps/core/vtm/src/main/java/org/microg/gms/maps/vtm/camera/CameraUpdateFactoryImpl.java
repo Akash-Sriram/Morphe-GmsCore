6c397d3b8b7097695c1f3a4510519828332fa80d
Akash Sriram <akashsriram@users.noreply.github.com>
2026-08-26 21:23:55 +0530

    fix(camera): safely deserialize LatLng and CameraPosition parcelables in CameraUpdateFactory onTransact

--- play-services-maps/core/vtm/src/main/java/org/microg/gms/maps/vtm/camera/CameraUpdateFactoryImpl.java
+++ play-services-maps/core/vtm/src/main/java/org/microg/gms/maps/vtm/camera/CameraUpdateFactoryImpl.java
@@ -200,4 +200,119 @@ public class CameraUpdateFactoryImpl extends ICameraUpdateFactoryDelegate.Stub {
             }
         });
     }
+
+    private static final String DESCRIPTOR = "com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate";
+    private static final int TRANSACTION_zoomIn = FIRST_CALL_TRANSACTION + 0;
+    private static final int TRANSACTION_zoomOut = FIRST_CALL_TRANSACTION + 1;
+    private static final int TRANSACTION_scrollBy = FIRST_CALL_TRANSACTION + 2;
+    private static final int TRANSACTION_zoomTo = FIRST_CALL_TRANSACTION + 3;
+    private static final int TRANSACTION_zoomBy = FIRST_CALL_TRANSACTION + 4;
+    private static final int TRANSACTION_zoomByWithFocus = FIRST_CALL_TRANSACTION + 5;
+    private static final int TRANSACTION_newCameraPosition = FIRST_CALL_TRANSACTION + 6;
+    private static final int TRANSACTION_newLatLng = FIRST_CALL_TRANSACTION + 7;
+    private static final int TRANSACTION_newLatLngZoom = FIRST_CALL_TRANSACTION + 8;
+    private static final int TRANSACTION_newLatLngBounds = FIRST_CALL_TRANSACTION + 9;
+    private static final int TRANSACTION_newLatLngBoundsWithSize = FIRST_CALL_TRANSACTION + 10;
+
+    @SuppressWarnings("unchecked")
+    private static <T> T readParcelableSafely(android.os.Parcel data, String className) {
+        if (data.readInt() == 0) return null;
+        try {
+            Class<?> clazz = Class.forName(className);
+            android.os.Parcelable.Creator<?> creator = (android.os.Parcelable.Creator<?>) clazz.getField("CREATOR").get(null);
+            return (T) creator.createFromParcel(data);
+        } catch (Throwable t) {
+            Log.w(TAG, "Failed to read parcelable: " + className, t);
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws RemoteException {
+        if (code == TRANSACTION_zoomIn) {
+            data.enforceInterface(DESCRIPTOR);
+            IObjectWrapper result = this.zoomIn();
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_zoomOut) {
+            data.enforceInterface(DESCRIPTOR);
+            IObjectWrapper result = this.zoomOut();
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_scrollBy) {
+            data.enforceInterface(DESCRIPTOR);
+            float arg0 = data.readFloat();
+            float arg1 = data.readFloat();
+            IObjectWrapper result = this.scrollBy(arg0, arg1);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_zoomTo) {
+            data.enforceInterface(DESCRIPTOR);
+            float arg0 = data.readFloat();
+            IObjectWrapper result = this.zoomTo(arg0);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_zoomBy) {
+            data.enforceInterface(DESCRIPTOR);
+            float arg0 = data.readFloat();
+            IObjectWrapper result = this.zoomBy(arg0);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_zoomByWithFocus) {
+            data.enforceInterface(DESCRIPTOR);
+            float arg0 = data.readFloat();
+            int arg1 = data.readInt();
+            int arg2 = data.readInt();
+            IObjectWrapper result = this.zoomByWithFocus(arg0, arg1, arg2);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_newCameraPosition) {
+            data.enforceInterface(DESCRIPTOR);
+            CameraPosition arg0 = readParcelableSafely(data, "com.google.android.gms.maps.model.CameraPosition");
+            IObjectWrapper result = this.newCameraPosition(arg0);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_newLatLng) {
+            data.enforceInterface(DESCRIPTOR);
+            LatLng arg0 = readParcelableSafely(data, "com.google.android.gms.maps.model.LatLng");
+            IObjectWrapper result = this.newLatLng(arg0);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_newLatLngZoom) {
+            data.enforceInterface(DESCRIPTOR);
+            LatLng arg0 = readParcelableSafely(data, "com.google.android.gms.maps.model.LatLng");
+            float arg1 = data.readFloat();
+            IObjectWrapper result = this.newLatLngZoom(arg0, arg1);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_newLatLngBounds) {
+            data.enforceInterface(DESCRIPTOR);
+            LatLngBounds arg0 = readParcelableSafely(data, "com.google.android.gms.maps.model.LatLngBounds");
+            int arg1 = data.readInt();
+            IObjectWrapper result = this.newLatLngBounds(arg0, arg1);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        } else if (code == TRANSACTION_newLatLngBoundsWithSize) {
+            data.enforceInterface(DESCRIPTOR);
+            LatLngBounds arg0 = readParcelableSafely(data, "com.google.android.gms.maps.model.LatLngBounds");
+            int arg1 = data.readInt();
+            int arg2 = data.readInt();
+            int arg3 = data.readInt();
+            IObjectWrapper result = this.newLatLngBoundsWithSize(arg0, arg1, arg2, arg3);
+            reply.writeNoException();
+            reply.writeStrongBinder(result != null ? result.asBinder() : null);
+            return true;
+        }
+        return super.onTransact(code, data, reply, flags);
+    }
 }
